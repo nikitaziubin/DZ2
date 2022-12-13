909ee672627c0bf2e2a6cef78447b0568e272475
nikita.ziubin <nikita.ziubin@gmail.com>
2022-12-13 19:17:28 +0200

    Add project files.

---
+++ Manager.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System;
+
+namespace DZ2
+{
+    internal class Manager : Worker
+    {
+        private Random rnd = new Random();
+           
+        public Manager(string name) : base(name)
+        {
+            position = "Manager";            
+        }
+        public override void FillWorkDay()
+        {
+            int random = rnd.Next(1, 10);
+            for (int i = 0; i < random; i++)
+            {
+                Call();
+            }
+            Relax();
+            random = rnd.Next(1, 5);
+            for (int i = 0; i < random; i++)
+            {
+                Call();
+            }
+        }
+
+    }
+}
